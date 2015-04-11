0875c92af7d058ed87106aa93085b6eb0ea95f42
Joseph Ruane <jeruane@gmail.com>
2015-04-11 11:56:52 -0400

    basic geolocation working
    map centers on geolocation of the user.

--- jstempname.js
+++ jstempname.js
@@ -1,52 +1,39 @@
-// This example creates circles on the map, representing
-// populations in North America.
-
-// First, create an object containing LatLng and population for each city.
-var citymap = {};
-citymap['chicago'] = {
-  center: new google.maps.LatLng(41.878113, -87.629798),
-  population: 2714856
-};
-citymap['newyork'] = {
-  center: new google.maps.LatLng(40.714352, -74.005973),
-  population: 8405837
-};
-citymap['losangeles'] = {
-  center: new google.maps.LatLng(34.052234, -118.243684),
-  population: 3857799
-};
-citymap['vancouver'] = {
-  center: new google.maps.LatLng(49.25, -123.1),
-  population: 603502
-};
-
-var cityCircle;
+var boston = new google.maps.LatLng(42.3601, -71.0589);
 
 function initialize() {
   // Create the map.
   var mapOptions = {
-    zoom: 4,
-    center: new google.maps.LatLng(37.09024, -95.712891),
+    zoom: 10,
+    center: boston,
     mapTypeId: google.maps.MapTypeId.TERRAIN
   };
 
   var map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
 
-  // Construct the circle for each value in citymap.
-  // Note: We scale the area of the circle based on the population.
-  for (var city in citymap) {
-    var populationOptions = {
-      strokeColor: '#FF0000',
-      strokeOpacity: 0.8,
-      strokeWeight: 2,
-      fillColor: '#FF0000',
-      fillOpacity: 0.35,
-      map: map,
-      center: citymap[city].center,
-      radius: Math.sqrt(citymap[city].population) * 100
-    };
-    // Add the circle for this city to the map.
-    cityCircle = new google.maps.Circle(populationOptions);
+  // Try W3C Geolocation (Preferred)
+  if(navigator.geolocation) {
+    browserSupportFlag = true;
+    navigator.geolocation.getCurrentPosition(function(position) {
+      initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+      map.setCenter(initialLocation);
+    }, function() {
+      handleNoGeolocation(browserSupportFlag);
+    });
+  }
+  // Browser doesn't support Geolocation
+  else {
+    browserSupportFlag = false;
+    handleNoGeolocation(browserSupportFlag);
+  }
+  
+  function handleNoGeolocation(errorFlag) {
+    if (errorFlag == true) {
+      alert("Geolocation service failed.");
+      initialLocation = boston;
+    } else {
+      alert("Your browser doesn't support geolocation. We've placed you in Boston.");
+    }
+    map.setCenter(initialLocation);
   }
 }
