eba813650b21d7020ac273e159c26ddc82b50188
Joseph Ruane <jeruane@gmail.com>
2015-04-11 13:52:31 -0400

    reformatted CSV + CSV reader in JS
    eyyyyyyyyyyyy bb

--- scripts/jstempname.js
+++ scripts/jstempname.js
@@ -1,4 +1,5 @@
 var initialLocation;
+var pathToAsteroidsFile = 'mockNearEarthAstroids.csv';
 var browserSupportFlag =  new Boolean();
 var maptype = google.maps.MapTypeId.HYBRID
 var bostonLocation = new google.maps.LatLng(42.3601, -71.0589);
@@ -57,10 +58,10 @@ function initialize() {
   function handleNoGeolocation(errorFlag) {
     if (errorFlag == true) {
       alert("Geolocation service failed.");
-      initialLocation = newyork;
+      initialLocation = bostonLocation;
     } else {
       alert("Your browser doesn't support geolocation. We've placed you in Siberia.");
-      initialLocation = siberia;
+      initialLocation = bostonLocation;
     }
     map.setCenter(initialLocation);
     var marker1 = new google.maps.Marker({
@@ -71,6 +72,8 @@ function initialize() {
     });
   }
   
+  readTextFile(pathToAsteroidsFile);
+  
   var marker2 = new google.maps.Marker({
       position: bostonLocation,
       map: map,
@@ -79,4 +82,50 @@ function initialize() {
       title: 'BOSTON CENTER',
       zIndex: 1
   });
+}
+
+function readTextFile(file) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                var csvArray = csvToArray(allText)
+                alert(csvArray[0]);
+            }
+        }
+    }
+    rawFile.send(null);
+}
+
+function csvToArray(csvString) {
+  // The array we're going to build
+  var csvArray   = [];
+  // Break it into rows to start
+  var csvRows    = csvString.split(/\n/);
+  // Take off the first line to get the headers, then split that into an array
+  var csvHeaders = csvRows.shift().split(',');
+
+  // Loop through remaining rows
+  for(var rowIndex = 0; rowIndex < csvRows.length; ++rowIndex){
+    var rowArray  = csvRows[rowIndex].split(',');
+
+    // Create a new row object to store our data.
+    var rowObject = csvArray[rowIndex] = {};
+    
+    // Then iterate through the remaining properties and use the headers as keys
+    for(var propIndex = 0; propIndex < rowArray.length; ++propIndex){
+      // Grab the value from the row array we're looping through...
+      var propValue = rowArray[propIndex].replace(/^"|"$/g,'');
+      // ...also grab the relevant header (the RegExp in both of these removes quotes)
+      var propLabel = csvHeaders[propIndex].replace(/^"|"$/g,'');;
+
+      rowObject[propLabel] = propValue;
+    }
+  }
+  return csvArray;
 }
